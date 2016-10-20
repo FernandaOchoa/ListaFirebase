f689becc38b510d9adf40472cda7e06331eb9d5c
Fernanda Ochoa <monc_sip@hotmail.com>
2016-10-20 18:21:53 -0500

    Add libraries

--- app/src/main/java/monsh/com/listafirebase/ToDoApplication.java
+++ app/src/main/java/monsh/com/listafirebase/ToDoApplication.java
@@ -1,7 +1,10 @@
 package monsh.com.listafirebase;
 
+import android.app.Application;
+
 /**
  * Created by monsh on 19/10/2016.
  */
-public class ToDoApplication {
+public class ToDoApplication extends Application {
+
 }
